f05db46b2bdc012de9a17a9486f871afe82ddc96
Simon Goričar <simon.peter.goricar@gmail.com>
2022-05-28 20:20:00 +0200

    Refactor and document users_router.py

--- v1/users/users_router.py
+++ v1/users/users_router.py
@@ -1,4 +1,5 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
+from typing import Optional
 
 from fastapi import APIRouter, Depends, Request, HTTPException
 from fastapi.encoders import jsonable_encoder
@@ -9,12 +10,11 @@ from starlette import status
 from starlette.datastructures import QueryParams
 from starlette.responses import JSONResponse
 
-
-import core.message_types as mt
 from core.exceptions import GeneralBackendException
+from core.message_types import Message
 from core.models.database import async_session
-from core.schemas.users_schema import *
 from core.configuration import config
+from core.schemas.users_schema import TokenData, User, UserDetail, UserCreate, UserUpdate, Role, UserLogin
 
 import v1.doc_strings as doc_str
 from v1.dependencies import oauth2_scheme
@@ -25,117 +25,190 @@ router = APIRouter(
     tags=["Users"]
 )
 
-pwd_context = CryptContext(schemes=['bcrypt'])
+pwd_context = CryptContext(schemes=["bcrypt"])
 
-
-async def get_user_dal():
+####
+# Utility functions
+####
+async def get_user_dal() -> UserDAL:
+    """
+    FastAPI injectable to provide access to users in the database.
+    """
     async with async_session() as session:
         async with session.begin():
-            yield UserDAL(session)
+            return UserDAL(session)
 
 
-async def verify_password(plain_password: str, hashed_password: str):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Verify the validity of the password by comparing it to a hash of the expected password.
+
+    :param plain_password: Plain-text password (will be hashed and compared).
+    :param hashed_password: Pasword hash to compare to.
+    :return: Boolean indicating whether the password matched the hash.
+    """
     try:
         return pwd_context.verify(plain_password, hashed_password)
     except (ValueError, TypeError):
         return False
 
 
-async def get_password_hash(password: str):
+def hash_password(password: str) -> str:
+    """
+    Hash the provided password.
+
+    :param password: Password to hash.
+    :return: Hashed password.
+    """
     return pwd_context.hash(password)
 
 
-async def authenticate_user(username: str, password: str, db: UserDAL):
-    user = await db.get_user_credentials_by_username(username)
-    if (not user or
-            not await verify_password(password, user.password)):
-        return False
+async def authenticate_user(username: str, password: str, database: UserDAL) -> Optional[UserLogin]:
+    """
+    Attempt to authenticate the user.
+
+    :param username: Username to authenticate with.
+    :param password: Password to authenticase with.
+    :param database: UserDAL instance to use as the data access layer.
+    :return: UserLogin instance if sucessfully authenticated, None otherwise.
+    """
+    user: Optional[UserLogin] = await database.get_user_credentials_by_username(username)
+    is_valid_password: bool = verify_password(password, user.password)
+
+    if user is None or is_valid_password is False:
+        return None
+
     return user
 
 
-async def get_current_user(db: UserDAL = Depends(get_user_dal),
-                           token: str = Depends(oauth2_scheme)):
+async def get_current_user(
+        database: UserDAL = Depends(get_user_dal),
+        token: str = Depends(oauth2_scheme)
+) -> UserDetail:
+    """
+    FastAPI injectable to fetch information about the authenticated user (via Bearer token).
+
+    :param database: Instance of UserDAL to access users.
+    :param token: Token from the Authentication header.
+    :return: UserDetail instance of the logged-in user.
+    """
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={'WWW-Authenticate': 'Bearer'}
+        headers={"WWW-Authenticate": "Bearer"}
     )
 
     try:
-        payload = jwt.decode(token, config.JWT.SECRET_KEY, algorithms=[config.JWT.ALGORITHM])
-        username: str = payload.get('sub')
+        payload = jwt.decode(token=token, key=config.JWT.SECRET_KEY, algorithms=[config.JWT.ALGORITHM])
+
+        username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
 
-    user = await db.get_user_by_username(token_data.username)
+        token_data = TokenData(username=username)
+    except JWTError as error:
+        raise credentials_exception from error
+
+    user = await database.get_user_by_username(token_data.username)
     if user is None:
         raise credentials_exception
 
     return user
 
 
-async def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.datetime.utcnow() + timedelta(minutes=15)
+def create_access_token(
+        data: dict,
+        expires_delta: timedelta = timedelta(minutes=15)
+) -> str:
+    """
+    Create an access token for the user.
 
-    to_encode.update({'exp': expire})
+    :param data: Data to encode in the JWT.
+    :param expires_delta: Timedelta of token expiration.
+    :return: Access token.
+    """
+    to_encode: dict = data.copy()
+    expire: datetime = datetime.utcnow() + expires_delta
+
+    to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, config.JWT.SECRET_KEY, algorithm=config.JWT.ALGORITHM)
 
     return encoded_jwt
 
 
-@router.get("/", response_model=list[User], status_code=200,
+####
+# Routes
+####
+@router.get("/",
+            response_model=list[User],
+            status_code=200,
             description=doc_str.GET_USERS)
-async def read_users(req: Request, db: UserDAL = Depends(get_user_dal)):
+async def read_users(
+        req: Request,
+        database: UserDAL = Depends(get_user_dal)
+):
     params = req.query_params
-    content, count = await db.get_users(params)
+    content, count = await database.get_users(params)
 
     json_content = jsonable_encoder(content)
-    headers = {"X-Total-Count": str(count)}
+    headers = {
+        "X-Total-Count": str(count)
+    }
 
     return JSONResponse(
         content=json_content, headers=headers
     )
 
 
-@router.post("/", response_model=UserDetail, status_code=201,
-             responses={400: {}}, description=doc_str.POST_USER)
-async def create_user(user: UserCreate, db: UserDAL = Depends(get_user_dal)):
-    user.password = await get_password_hash(user.password)
-    new_user = await db.create_user(user)
+@router.post("/",
+             response_model=UserDetail,
+             status_code=201,
+             responses={400: {}},
+             description=doc_str.POST_USER)
+async def create_user(
+        new_user: UserCreate,
+        database: UserDAL = Depends(get_user_dal)
+):
+    new_user.password = hash_password(new_user.password)
+
+    new_user = await database.create_user(new_user)
     if new_user is None:
         raise GeneralBackendException(400, "Username already taken")
+
     return new_user
 
 
-@router.get("/me", response_model=UserDetail)
-async def read_user_me(current_user: User = Depends(get_current_user)):
+@router.get("/me",
+            response_model=UserDetail)
+async def read_user_me(
+        current_user: User = Depends(get_current_user)
+):
     return current_user
 
 
 @router.get("/me/perms")
-async def read_user_me_permissions(current_user: User = Depends(get_current_user),
-                                   db: UserDAL = Depends(get_user_dal)):
-    roles, _ = await db.get_user_roles(current_user.id, None)
+async def read_user_me_permissions(
+        current_user: User = Depends(get_current_user),
+        database: UserDAL = Depends(get_user_dal)
+):
+    roles, _ = await database.get_user_roles(current_user.id, None)
     perms = 0
     for role in roles:
         perms = perms | role.permissions
 
     return {
-        'permissions': perms
+        "permissions": perms
     }
 
 
-@router.post("/token", description=doc_str.LOGIN)
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: UserDAL = Depends(get_user_dal)):
-    user = await authenticate_user(form_data.username, form_data.password, db)
-    if not user:
+@router.post("/token",
+             description=doc_str.LOGIN)
+async def login(
+        form_data: OAuth2PasswordRequestForm = Depends(),
+        database: UserDAL = Depends(get_user_dal)
+):
+    user = await authenticate_user(form_data.username, form_data.password, database)
+    if user is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
@@ -143,8 +216,9 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: UserDAL = 
         )
 
     access_token_expires = timedelta(minutes=config.JWT.ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = await create_access_token(
-        data={'sub': user.username}, expires_delta=access_token_expires
+    access_token = create_access_token(
+        data={'sub': user.username},
+        expires_delta=access_token_expires
     )
 
     return {
@@ -153,44 +227,64 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: UserDAL = 
     }
 
 
-@router.get("/{user_id}", response_model=UserDetail, status_code=200,
-            responses={404: {}}, description=doc_str.GET_USER)
-async def read_user_details(user_id: int, db: UserDAL = Depends(get_user_dal)):
-    user = await db.get_user(user_id)
+@router.get("/{user_id}",
+            response_model=UserDetail,
+            status_code=200,
+            responses={404: {}},
+            description=doc_str.GET_USER)
+async def read_user_details(
+        user_id: int,
+        database: UserDAL = Depends(get_user_dal)
+):
+    user = await database.get_user(user_id)
     if not user:
         raise GeneralBackendException(404, "User not found")
     return user
 
 
-@router.put("/{user_id}", response_model=mt.Message, status_code=200,
-            responses={404: {}, 400: {}}, description=doc_str.PUT_USER)
-async def update_user(user_data: UserUpdate, user_id: int, db: UserDAL = Depends(get_user_dal),
-                      current_user: UserDetail = Depends(get_current_user)):
+@router.put("/{user_id}",
+            response_model=Message,
+            status_code=200,
+            responses={404: {}, 400: {}},
+            description=doc_str.PUT_USER)
+async def update_user(
+        user_data: UserUpdate,
+        user_id: int,
+        database: UserDAL = Depends(get_user_dal),
+        current_user: UserDetail = Depends(get_current_user)
+):
     # TODO: Admin permission
     if user_id != current_user.id:
         raise GeneralBackendException(401, "You are not the owner of this account.")
 
-    updated, msg = await db.update_user(user_data, user_id)
+    updated, msg = await database.update_user(user_data, user_id)
     if not updated:
         raise GeneralBackendException(404, msg)
 
-    return mt.Message(detail=msg)
+    return Message(detail=msg)
 
 
 @router.delete("/{user_id}", status_code=200,
                responses={404: {}}, description=doc_str.DELETE_USER)
-async def remove_user(user_id: int, db: UserDAL = Depends(get_user_dal)):
-    deleted = await db.delete_user(user_id)
+async def remove_user(
+        user_id: int,
+        database: UserDAL = Depends(get_user_dal)
+):
+    deleted = await database.delete_user(user_id)
     if not deleted:
         raise GeneralBackendException(404, "User not found")
-    return mt.Message(detail="User deleted")
+    return Message(detail="User deleted")
 
 
 @router.get("/{user_id}/roles", response_model=list[Role], status_code=200,
             description=doc_str.GET_USER_ROLES)
-async def read_user_roles(req: Request, user_id: int, db: UserDAL = Depends(get_user_dal)):
+async def read_user_roles(
+        req: Request,
+        user_id: int,
+        database: UserDAL = Depends(get_user_dal)
+):
     params: QueryParams = req.query_params
-    content, count = await db.get_user_roles(user_id, params)
+    content, count = await database.get_user_roles(user_id, params)
 
     json_content = jsonable_encoder(content)
     headers = {"X-Total-Count": str(count)}
@@ -200,19 +294,27 @@ async def read_user_roles(req: Request, user_id: int, db: UserDAL = Depends(get_
     )
 
 
-@router.post("/{user_id}/roles", response_model=mt.Message, status_code=200,
+@router.post("/{user_id}/roles", response_model=Message, status_code=200,
              description=doc_str.POST_USER_ROLES)
-async def add_user_roles(user_id: int, role_ids: list[int], db: UserDAL = Depends(get_user_dal)):
-    added, msg = await db.create_user_roles(user_id, role_ids)
+async def add_user_roles(
+        user_id: int,
+        role_ids: list[int],
+        database: UserDAL = Depends(get_user_dal)
+):
+    added, msg = await database.create_user_roles(user_id, role_ids)
     if not added:
         raise GeneralBackendException(404, msg)
-    return mt.Message(detail=msg)
+    return Message(detail=msg)
 
 
-@router.delete("/{user_id}/roles", response_model=mt.Message, status_code=200,
+@router.delete("/{user_id}/roles", response_model=Message, status_code=200,
                description=doc_str.DELETE_USER_ROLES)
-async def remove_user_roles(user_id: int, role_ids: list[int], db: UserDAL = Depends(get_user_dal)):
-    removed, msg = await db.remove_user_roles(user_id, role_ids)
+async def remove_user_roles(
+        user_id: int,
+        role_ids: list[int],
+        database: UserDAL = Depends(get_user_dal)
+):
+    removed, msg = await database.remove_user_roles(user_id, role_ids)
     if not removed:
         raise GeneralBackendException(404, msg)
-    return mt.Message(detail=msg)
+    return Message(detail=msg)
